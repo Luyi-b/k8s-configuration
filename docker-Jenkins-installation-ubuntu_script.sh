bbfa277ea1206bcb7fbb3380a820c36299cdad5d
aanuoluwapoakinyera <aanuoluwapoakinyera@gmail.com>
2022-03-27 01:27:01 -0400

    modified shell script

--- docker-Jenkins-installation-ubuntu_script.sh
+++ docker-Jenkins-installation-ubuntu_script.sh
@@ -5,7 +5,7 @@ sudo apt install docker.io -y
 sudo usermod -aG docker ubuntu 
 # Install java as jenkins dependency
 sudo apt install openjdk-11-jdk -y
-# install jenkis in ubuntu:
+# install jenkins in ubuntu:
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
     /etc/apt/sources.list.d/jenkins.list'
